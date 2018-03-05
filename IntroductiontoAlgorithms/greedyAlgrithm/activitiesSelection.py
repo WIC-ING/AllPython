df7444748b4f9545fbb40e14e719f8344c10e85c
huajian-liu <huajian_gl@163.com>
2018-03-05 23:05:16 +0800

    '贪心算法之活动选择问题'

---
+++ IntroductiontoAlgorithms/greedyAlgrithm/activitiesSelection.py
@@ -0,0 +1,12 @@
+# 贪心算法之活动选择问题
+
+# S表示不同活动的开始时间
+S = [1, 3, 0, 5, 3, 5,  6,  8,  8,  2, 12]
+# F表示不同活动的结束时间
+F = [4, 5, 6, 7, 9, 9, 10, 11, 12, 14, 16]
+
+
+# 函数名称：RECURSIVE_ACTIVITY_SELECTOR
+# 函数功能：递归贪心算法 之活动选择问题
+def RECURSIVE_ACTIVITY_SELECTOR(S, F, k, n):
+    pass
