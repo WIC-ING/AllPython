6f0e3f141951855cb0e29e2a4f2c68219a482c56
chunyu-lhj <liuhuajian@chunyu.me>
2018-03-07 14:01:04 +0800

    'charactor03 finished'

--- TF_Test_0306/charactor03/charactor3.4.py
+++ TF_Test_0306/charactor03/charactor3.4.py
@@ -1,8 +1,10 @@
+# -*- coding:utf-8 -*-
+import tensorflow as tf
+
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
-
-import tensorflow as tf
 from numpy.random import RandomState
+
 # 定义训练数据batch的大小
 batch_size = 8
 
@@ -25,7 +27,7 @@ y = tf.matmul(a, w2)
 # 定义损失函数和反向传播的算法
 cross_entropy = -tf.reduce_mean(
     y_ * tf.log(tf.clip_by_value(y, 1e-10, 1.0)))
-train_step = tf.train.AdadeltaOptimizer(0.001).minimize(cross_entropy)
+train_step = tf.train.AdadeltaOptimizer(1).minimize(cross_entropy)
 
 
 # 通过随机数生成一个模拟数据集
@@ -45,5 +47,31 @@ with tf.Session() as sess:
     print(sess.run(w2))
 
     """
-    
+    在训练之前神经网络参数的值：
+    W1 = 
+    W2 = 
     """
+
+    # 设定训练的轮数：
+    STEPS = 5000
+    for i in range(STEPS):
+        # 每次选取batch_size个样本进行训练
+        start = ( i * batch_size) % dataset_size
+        end   = min(start+batch_size, dataset_size)
+
+        # 通过选取的样本训练神经网络并更新参数
+        sess.run(train_step,
+                 feed_dict = {x:X[start:end], y_:Y[start:end]})
+        if i % 1000 == 0:
+            # 每隔一段时间计算在所有数据上的交叉熵并输出
+            total_cross_entropy = sess.run(
+                cross_entropy, feed_dict={x:X, y_:Y}
+            )
+            print("After %d traing step(s),cross entropy on all data is %g" % (i, total_cross_entropy))
+
+
+    print(sess.run(w1))
+    print(sess.run(w2))
+
+
+
